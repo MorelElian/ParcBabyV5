9e710d115dde0c3bdb3c0d645ac63bc01cccf39f
Elian Morel <elian.morel@laposte.net>
2022-06-05 13:05:28 +0200

    -deDridft

--- project/src/kart.cpp
+++ project/src/kart.cpp
@@ -383,7 +383,11 @@ void Kart::updateAccelerationKart(float pressForward)
 }
 void Kart::updateVitesseKart(float dt)
 {
-	vitesseKart = dt * accelerationKart + vitesseKart;
+	float scal = (dt * accelerationKart + vitesseKart).x * orientationKart.x + (dt * accelerationKart + vitesseKart).y * orientationKart.y
+		+ (dt * accelerationKart + vitesseKart).z * orientationKart.z;
+	vitesseKart.x = scal * orientationKart.x;
+	vitesseKart.y = scal* orientationKart.y;
+	vitesseKart.z =scal* orientationKart.z;
 }
 void Kart::udpatePositionKart(float pressForward,float dt)
 {
